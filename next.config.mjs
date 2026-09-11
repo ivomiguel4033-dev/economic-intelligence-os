e9b8eb82787d0259f7a09f86890778a4f81de4a6
Ivo oliveira <ivomiguel4033@gmail.com>
2026-09-11 02:11:12 +0100

    Enforce HTTPS with HSTS

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
           { key: 'X-DNS-Prefetch-Control', value: 'off' },
+          { key: 'Strict-Transport-Security', value: 'max-age=31536000' },
         ],
       },
     ];
